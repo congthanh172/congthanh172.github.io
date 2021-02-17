f88e964e32269da200b0f35bc9339a2a80b29487
Nguyen Cong Thanh <muahangonlineabc@gmail.com>
2021-02-17 15:07:41 +0700

    project

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// let postApi = "https://jsonplaceholder.typicode.com/posts";
+
+// fetch(postApi)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (posts) {
+//     let htmls = posts.map(function (post) {
+//       return `<li>
+//         <h2>${post.title}</h2>
+//       </li>`;
+//     });
+
+//     let html = htmls.join("");
+
+//     document.getElementById("copyright").innerHTML = html;
